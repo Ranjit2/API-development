6eb506f956d7186523326957f5019c8abde376dd
Ranjit2 <ranjitkarki2@gmail.com>
2022-07-13 17:36:19 +1000

    country population

--- app/Http/Controllers/CountryPopulationController.php
+++ app/Http/Controllers/CountryPopulationController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Population;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\CountryPopulationResource;
 
 class CountryPopulationController extends Controller
 {
-    public function index()
+    public function index(): JsonResource
     {
-        return  CountryPopulationResource::collection(Population::countryWithHighestPopulation()->get());
+        return CountryPopulationResource::collection(
+            Population::countryWithHighestPopulation()
+                ->get()
+        );
     }
 }
